148ee3ba1f74a93f342ffba8f70b263b7ad1baac
JoanneLin <j87101488@gmail.com>
2020-03-22 12:25:33 +0800

    Initial project

---
+++ business_meet/src/main/java/tw/com/business_meet/BusinessMeetApplication.java
@@ -0,0 +1,13 @@
+package tw.com.business_meet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessMeetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessMeetApplication.class, args);
+    }
+
+}
